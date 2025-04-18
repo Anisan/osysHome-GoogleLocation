2d7f96cd9feccdcab970207cdd65f8520a3efcd0
Eraser <eraser1981@gmail.com>
2025-04-18 11:57:26 +0300

    fix: table size image path

--- models/__init__.py
+++ models/__init__.py
@@ -5,7 +5,7 @@ class Location(SurrogatePK, db.Model):
     id_user = Column(db.String(255), nullable=False)
     name = Column(db.String(255), nullable=False)
     fullname = Column(db.String(255), nullable=False)
-    image = Column(db.String(255), nullable=False)
+    image = Column(db.String(2048), nullable=False)
     last_update = Column(db.DateTime())
     address = Column(db.String(512), nullable=True)
     lat = Column(db.Float(), default = 0.0)
